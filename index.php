e395c95a393cd6c223bd5fd1817c416c5309920b
adetolarge <adetolarge@gmail.com>
2024-11-07 08:00:49 +0100

    update

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
 <?php
 include "./data_manager.php";
-
-
-
 $submitted = isset($_POST["UserName"], $_POST["form1"]);
 if($submitted){
     $username =  $_POST["UserName"];
     echo $username;
     AddProducts($username, "https://www.sony.com/image/6145c1d32e6ac8e63a46c912dc33c5bb?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF", "Nice Headphones");
 }
-
 $submitted1 = isset($_POST["Delete"]);
 if($submitted1){
     DeleteProduct(0);
     header("Location:index.php");
 }
-
 $products = GetProducts();
-
 foreach($products as $key => $value) {
         ?>
-
-    
     <div style="border: solid 1px red;">
         <h3><?php echo $value["name"];?></h3>
         <img src="<?php echo $value["image"];?>" alt="" height="100px" width="100px">
@@ -31,12 +23,9 @@ foreach($products as $key => $value) {
     </div>
     
     <?php
- 
 }
 // var_dump(GetProducts());
 ?>
-
-
 <form action="" method="post" >
     <input type="text" name="UserName" placeholder="username">
     <button name="form1">Submit</button>
